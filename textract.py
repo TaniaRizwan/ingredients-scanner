472ad269db7b72ff277b897b54644f7382cec8af
hamza.siddiqui <hamza.siddiqui@ucalgary.ca>
2025-09-30 21:01:16 -0600

    Add textract.py initial version

---
+++ textract.py
@@ -0,0 +1,12 @@
+import boto3
+
+textract = boto3.client('textract') 
+
+with open('image2.jpeg', 'rb') as img:
+    document_bytes = img.read()
+
+    response = textract.detect_document_text(Document = {'Bytes': document_bytes})
+
+    for item in response['Blocks']:
+        if item['BlockType'] == 'LINE':
+            print(item['Text'])
